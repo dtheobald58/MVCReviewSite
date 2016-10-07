f185cfdc8725f58f18883e902bc153418beed65e
dtheobald58 <dtheobald58@yahoo.com>
2016-10-07 10:07:02 -0400

    added models

---
+++ WebApplication1/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public string ReviewSummary { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
